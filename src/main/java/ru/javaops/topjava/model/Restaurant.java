a311395ee940271a856f286b53d10b5b27909170
MaletyPerun <malety34@gmail.com>
2022-08-23 13:07:23 +0300

    Prepared all main models

--- src/main/java/ru/javaops/topjava/model/Restaurant.java
+++ src/main/java/ru/javaops/topjava/model/Restaurant.java
@@ -1,15 +1,18 @@
 package ru.javaops.topjava.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 import ru.javaops.topjava.util.validation.NoHtml;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 @Entity
 @Table(name = "restaurant")
@@ -25,10 +28,11 @@ public class Restaurant extends NamedEntity {
     @NoHtml
     private String address;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "meal_id", nullable = false)
-    @JsonIgnore
-    private Meal meal;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "restaurant")//, cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @OrderBy("price DESC")
+    @OnDelete(action = OnDeleteAction.CASCADE) //https://stackoverflow.com/a/44988100/548473
+    @Schema(hidden = true)
+    private List<Meal> meals;
 
     public Restaurant(Integer id, String name, String address) {
         super(id, name);
