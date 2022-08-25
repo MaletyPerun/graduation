6f7dad3a56d5c8437258c8199c40ce6a70550502
MaletyPerun <malety34@gmail.com>
2022-08-25 15:34:19 +0300

    Fix update Users

--- src/main/java/ru/topjava/util/UserUtil.java
+++ src/main/java/ru/topjava/util/UserUtil.java
@@ -17,7 +17,9 @@ public class UserUtil {
     public static User updateFromTo(User user, UserTo userTo) {
         user.setName(userTo.getName());
         user.setEmail(userTo.getEmail().toLowerCase());
-        user.setPassword(userTo.getPassword());
+        if (userTo.getPassword() != null) {
+            user.setPassword(userTo.getPassword());
+        }
         return user;
     }
 
--- src/main/java/ru/topjava/web/user/ProfileController.java
+++ src/main/java/ru/topjava/web/user/ProfileController.java
@@ -5,17 +5,21 @@ import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.topjava.model.User;
 import ru.topjava.to.UserTo;
 import ru.topjava.util.UserUtil;
 import ru.topjava.web.AuthUser;
 
 import javax.validation.Valid;
+import java.net.URI;
 
 import static ru.topjava.util.validation.ValidationUtil.assureIdConsistent;
+import static ru.topjava.util.validation.ValidationUtil.checkNew;
 
 @RestController
 @RequestMapping(value = ProfileController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -36,17 +40,17 @@ public class ProfileController extends AbstractUserController {
         super.delete(authUser.id());
     }
 
-//    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-//    @ResponseStatus(HttpStatus.CREATED)
-//    @CacheEvict(allEntries = true)
-//    public ResponseEntity<User> register(@Valid @RequestBody UserTo userTo) {
-//        log.info("register {}", userTo);
-//        checkNew(userTo);
-//        User created = prepareAndSave(UserUtil.createNewFromTo(userTo));
-//        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
-//                .path(REST_URL).build().toUri();
-//        return ResponseEntity.created(uriOfNewResource).body(created);
-//    }
+    @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    @CacheEvict(allEntries = true)
+    public ResponseEntity<User> register(@Valid @RequestBody UserTo userTo) {
+        log.info("register {}", userTo);
+        checkNew(userTo);
+        User created = prepareAndSave(UserUtil.createNewFromTo(userTo));
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL).build().toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
