fdfb76cc831d3e87e7642c5db316af61c0714534
MaletyPerun <malety34@gmail.com>
2022-09-03 21:24:22 +0300

    Rename App

--- src/main/java/ru/topjava/VoteApplication.java
+++ src/main/java/ru/topjava/VoteApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CaloriesManagementRestApplication {
+public class VoteApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CaloriesManagementRestApplication.class, args);
+        SpringApplication.run(VoteApplication.class, args);
     }
 }
