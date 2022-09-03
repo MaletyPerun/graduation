ff05461eb2fc4244ff91d37b54d5acc7df8d7e79
teplo <malety34@gmail.com>
2022-09-03 19:17:32 +0300

    ProfileServiceTest

---
+++ src/test/java/ru/topjava/service/AbstractServiceTest.java
@@ -0,0 +1,13 @@
+package ru.topjava.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+@ActiveProfiles("test")
+//https://docs.spring.io/spring-boot/docs/current/reference/html/spring-boot-features.html#boot-features-testing-spring-boot-applications-testing-with-mock-environment
+public abstract class AbstractServiceTest {
+
+}
